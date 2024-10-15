de9241569a3754ef3d9ad5258e34e1570f4d079f
Enes Kızılca <76004060+eneskizilca@users.noreply.github.com>
2024-10-16 00:12:31 +0300

    Add files via upload

---
+++ HangManGame/HangManGame/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace HangManGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.lblKelime = new System.Windows.Forms.Label();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.txtGirilenHarf = new System.Windows.Forms.TextBox();
+            this.lblHarfGiriniz = new System.Windows.Forms.Label();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.lblIpucu = new System.Windows.Forms.Label();
+            this.lblHak = new System.Windows.Forms.Label();
+            this.lblTamTahmin = new System.Windows.Forms.Label();
+            this.txtTamTahmin = new System.Windows.Forms.TextBox();
+            this.btnTamKontrol = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(445, 554);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // lblKelime
+            // 
+            this.lblKelime.AutoSize = true;
+            this.lblKelime.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKelime.Location = new System.Drawing.Point(552, 72);
+            this.lblKelime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblKelime.Name = "lblKelime";
+            this.lblKelime.Size = new System.Drawing.Size(317, 40);
+            this.lblKelime.TabIndex = 0;
+            this.lblKelime.Text = "0yuna Hoş Geldiniz!";
+            this.lblKelime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblKelime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnBaslat.Location = new System.Drawing.Point(615, 486);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(189, 54);
+            this.btnBaslat.TabIndex = 3;
+            this.btnBaslat.Text = "Oyunu Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = false;
+            this.btnBaslat.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtGirilenHarf
+            // 
+            this.txtGirilenHarf.Location = new System.Drawing.Point(764, 202);
+            this.txtGirilenHarf.Name = "txtGirilenHarf";
+            this.txtGirilenHarf.Size = new System.Drawing.Size(58, 39);
+            this.txtGirilenHarf.TabIndex = 4;
+            this.txtGirilenHarf.Visible = false;
+            // 
+            // lblHarfGiriniz
+            // 
+            this.lblHarfGiriniz.AutoSize = true;
+            this.lblHarfGiriniz.Location = new System.Drawing.Point(591, 205);
+            this.lblHarfGiriniz.Name = "lblHarfGiriniz";
+            this.lblHarfGiriniz.Size = new System.Drawing.Size(167, 35);
+            this.lblHarfGiriniz.TabIndex = 5;
+            this.lblHarfGiriniz.Text = "Harf Giriniz:";
+            this.lblHarfGiriniz.Visible = false;
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnKontrol.Location = new System.Drawing.Point(597, 257);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(225, 49);
+            this.btnKontrol.TabIndex = 6;
+            this.btnKontrol.Text = "Kontrol";
+            this.btnKontrol.UseVisualStyleBackColor = false;
+            this.btnKontrol.Visible = false;
+            this.btnKontrol.Click += new System.EventHandler(this.bntKontrol_Click);
+            // 
+            // lblIpucu
+            // 
+            this.lblIpucu.AutoSize = true;
+            this.lblIpucu.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIpucu.Location = new System.Drawing.Point(581, 134);
+            this.lblIpucu.Name = "lblIpucu";
+            this.lblIpucu.Size = new System.Drawing.Size(72, 27);
+            this.lblIpucu.TabIndex = 7;
+            this.lblIpucu.Text = "İpucu:";
+            this.lblIpucu.Visible = false;
+            // 
+            // lblHak
+            // 
+            this.lblHak.AutoSize = true;
+            this.lblHak.Location = new System.Drawing.Point(626, 438);
+            this.lblHak.Name = "lblHak";
+            this.lblHak.Size = new System.Drawing.Size(168, 35);
+            this.lblHak.TabIndex = 8;
+            this.lblHak.Text = "Kalan Hak: 7";
+            this.lblHak.Visible = false;
+            // 
+            // lblTamTahmin
+            // 
+            this.lblTamTahmin.AutoSize = true;
+            this.lblTamTahmin.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTamTahmin.Location = new System.Drawing.Point(532, 352);
+            this.lblTamTahmin.Name = "lblTamTahmin";
+            this.lblTamTahmin.Size = new System.Drawing.Size(347, 23);
+            this.lblTamTahmin.TabIndex = 9;
+            this.lblTamTahmin.Text = "(Kelimeyi tahmin etmek istiyorsan aşağı yazabiliirsin!)";
+            this.lblTamTahmin.Visible = false;
+            // 
+            // txtTamTahmin
+            // 
+            this.txtTamTahmin.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTamTahmin.Location = new System.Drawing.Point(597, 389);
+            this.txtTamTahmin.Name = "txtTamTahmin";
+            this.txtTamTahmin.Size = new System.Drawing.Size(178, 35);
+            this.txtTamTahmin.TabIndex = 10;
+            this.txtTamTahmin.Visible = false;
+            this.txtTamTahmin.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnTamKontrol
+            // 
+            this.btnTamKontrol.BackColor = System.Drawing.Color.Lime;
+            this.btnTamKontrol.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTamKontrol.Location = new System.Drawing.Point(781, 389);
+            this.btnTamKontrol.Name = "btnTamKontrol";
+            this.btnTamKontrol.Size = new System.Drawing.Size(41, 35);
+            this.btnTamKontrol.TabIndex = 11;
+            this.btnTamKontrol.Text = "✓";
+            this.btnTamKontrol.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTamKontrol.UseVisualStyleBackColor = false;
+            this.btnTamKontrol.Visible = false;
+            this.btnTamKontrol.Click += new System.EventHandler(this.btnTamKontrol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(951, 578);
+            this.Controls.Add(this.btnTamKontrol);
+            this.Controls.Add(this.txtTamTahmin);
+            this.Controls.Add(this.lblTamTahmin);
+            this.Controls.Add(this.lblHak);
+            this.Controls.Add(this.lblIpucu);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.lblHarfGiriniz);
+            this.Controls.Add(this.txtGirilenHarf);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.lblKelime);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HangMan Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label lblKelime;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.TextBox txtGirilenHarf;
+        private System.Windows.Forms.Label lblHarfGiriniz;
+        private System.Windows.Forms.Button btnKontrol;
+        private System.Windows.Forms.Label lblIpucu;
+        private System.Windows.Forms.Label lblHak;
+        private System.Windows.Forms.Label lblTamTahmin;
+        private System.Windows.Forms.TextBox txtTamTahmin;
+        private System.Windows.Forms.Button btnTamKontrol;
+    }
+}
+
---
+++ HangManGame/HangManGame/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangManGame
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        string secilenKelime;
+        char[] secilenKelimeCharDizisi;
+        string gizlenmisKelime = string.Empty;
+        int konuNumarasi;
+        int hak;
+        int sayac;
+
+        string image1 = Path.Combine(Application.StartupPath, "images", "1.png");
+
+        string[] sehirler = {
+            "ADANA", "ADIYAMAN", "AFYONKARAHİSAR", "AĞRI", "AMASYA", "ANKARA", "ANTALYA", "ARTVİN", "AYDIN",
+            "BALIKESİR", "BİLECİK", "BİNGÖL", "BİTLİS", "BOLU", "BURDUR", "BURSA", "ÇANAKKALE", "ÇANKIRI",
+            "ÇORUM", "DENİZLİ", "DİYARBAKIR", "EDİRNE", "ELAZIĞ", "ERZİNCAN", "ERZURUM", "ESKİŞEHİR",
+            "GAZİANTEP", "GİRESUN", "GÜMÜŞHANE", "HAKKARİ", "HATAY", "IĞDIR", "ISPARTA", "İSTANBUL",
+            "İZMİR", "KARS", "KASTAMONU", "KAYSERİ", "KIRIKKALE", "KIRKLARELİ", "KIRŞEHİR", "KİLİS",
+            "KONYA", "KÜTAHYA", "MALATYA", "MANİSA", "MARDİN", "MERSİN", "MUĞLA", "MUŞ", "NEVŞEHİR",
+            "NİĞDE", "ORDU", "OSMANİYE", "RİZE", "SAKARYA", "SAMSUN", "SİİRT", "SİNOP", "SİVAS",
+            "TEKİRDAĞ", "TOKAT", "TRABZON", "TUNCELİ", "ŞANLIURFA", "UŞAK", "VAN", "YALOVA", "YOZGAT",
+            "ZONGULDAK"
+        };
+
+        string[] yemekler = {
+            "İSKENDER KEBAP", "LAHMACUN", "MANTI", "ADANA KEBAP", "BEYTİ", "KARNIYARIK",
+            "HÜNKAR BEĞENDİ", "PİLAV", "DÖNER", "MENEMEN", "YAPRAK SARMA", "ÇİĞ KÖFTE",
+            "PATLICAN MUSAKKA", "KUZU TANDIR", "İMAM BAYILDI", "TAVUK ŞİŞ", "ÇORBA", "FIRIN KEBABI",
+            "ALİNAZİK", "MÜCVER", "KÖFTE", "GÖZLEME", "KUZU GÜVEÇ", "PİDE", "KURU FASULYE",
+            "BULGUR PİLAVI", "MIHLAMA", "TESTİ KEBABI", "ETLİ EKMEK", "CACIK"
+        };
+
+        string[] tatlilar = {
+            "BAKLAVA", "KÜNEFE", "REVANİ", "AŞURE", "SÜTLAÇ",
+            "KAZANDİBİ", "GÜLLAÇ", "ŞEKERPARE", "TULUMBA", "İRMİK HELVASI",
+            "KADAYIF", "LOKMA", "KEŞKÜL", "TAVUKGÖĞSÜ", "FIRIN SÜTLAÇ",
+            "HÖŞMERİM", "EKMEK KADAYIFI", "UN HELVASI", "SUPANGLE",
+            "ZERDE", "KEMALPAŞA", "TRİLEÇE", "DONDURMA",
+            "AYVA TATLISI", "KABAK TATLISI", "HANIMGÖBEĞİ",
+            "ORCİK", "PESTİL", "SAMSATATLI", "TEL KADAYIF"
+        };
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            string kapakFotosu = Path.Combine(Application.StartupPath, "images", "kapak_foto.png");
+            pictureBox.Image = Image.FromFile(kapakFotosu);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblIpucu.Visible = true;
+            lblHak.Visible = true;
+            lblHarfGiriniz.Visible = true;
+            btnKontrol.Visible = true;
+            txtGirilenHarf.Visible = true;
+            lblTamTahmin.Visible = true;
+            txtTamTahmin.Visible = true;
+            btnTamKontrol.Visible = true;
+            this.BackColor = Color.White;
+            hak = 7;
+            gizlenmisKelime = "";
+            txtGirilenHarf.Text = "";
+            txtTamTahmin.Text = "";
+            konuNumarasi = random.Next(1, 4);
+            switch (konuNumarasi)
+            {
+                case 1:
+                    secilenKelime = sehirler[random.Next(0, 81)];
+                    lblIpucu.Text = "İpucu: Bir şehir!";
+                    break;
+                case 2:
+                    secilenKelime = yemekler[random.Next(0, 30)];
+                    lblIpucu.Text = "İpucu: Bir yemek!";
+                    break;
+                case 3:
+                    secilenKelime = tatlilar[random.Next(0, 30)];
+                    lblIpucu.Text = "İpucu: Bir tatlı!";
+                    break;
+            }
+            lblHak.Text = "Kalan Hak: " + hak;
+            pictureBox.Image = Image.FromFile(image1);
+            secilenKelimeCharDizisi = secilenKelime.ToCharArray();
+            sayac = secilenKelime.Length;
+            for (int i = 0; i < secilenKelime.Length; i++)
+            {
+                if (secilenKelime[i] == ' ')
+                    sayac--;
+            }
+
+            for (int i = 0; i < secilenKelime.Length; i++)
+            {
+                if (secilenKelime[i] != ' ')
+                {
+                    secilenKelimeCharDizisi[i] = '⊡';
+                }
+            }
+            lblKelime.Text = string.Concat(secilenKelimeCharDizisi);
+        }
+
+        private void bntKontrol_Click(object sender, EventArgs e)
+        {
+            if (hak > 0 && !(string.IsNullOrEmpty(txtGirilenHarf.Text)))
+            {
+                char girilenHarf = txtGirilenHarf.Text[0];
+                bool harfBulundu = false;
+                txtGirilenHarf.Text = "";
+                for (int i = 0; i < secilenKelime.Length; i++)
+                {
+                    if (secilenKelime[i] == char.ToUpper(girilenHarf))
+                    {
+                        secilenKelimeCharDizisi[i] = char.ToUpper(Convert.ToChar(girilenHarf));
+                        lblKelime.Text = string.Concat(secilenKelimeCharDizisi);
+                        harfBulundu = true;
+                        sayac--;
+                        if (sayac == 0)
+                        {
+                            this.BackColor = Color.Green;
+                            MessageBox.Show("Tebrikler Kazandınız !");
+                            btnBaslat.PerformClick();
+                            break;
+                        }
+                    }
+                }
+
+                if ((!harfBulundu) && sayac != 0)
+                {
+                    hak--;
+                    lblHak.Text = "Kalan Hak: " + hak;
+                    int deneme = 8 - hak;
+                    string deneme2 = deneme + ".png";
+                    string image = Path.Combine(Application.StartupPath, "images", deneme2);
+                    pictureBox.Image = Image.FromFile(image);
+                    if (hak == 0)
+                    {
+                        this.BackColor = Color.Red;
+                        MessageBox.Show("Oyunu Kaybettiniz ! Doğru cevap: " + secilenKelime);
+                        btnBaslat.PerformClick();
+                    }
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTamKontrol_Click(object sender, EventArgs e)
+        {
+
+            if (txtTamTahmin.Text.ToUpper().Equals(secilenKelime))
+            {
+                kazanmaAnimasyonu();
+            }
+            else
+            {
+                MessageBox.Show("Yanlış tahmin !");
+            }
+        }
+
+        private void kazanmaAnimasyonu()
+        {
+            lblKelime.Text = secilenKelime;
+            this.BackColor = Color.Green;
+            MessageBox.Show("Tebrikler Kazandınız !");
+            btnBaslat.PerformClick();
+        }
+    }
+}
